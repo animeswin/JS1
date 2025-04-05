d00d9ad3d1e867d0e34903dd471a952ce895effb
animeswin <163599618+animeswin@users.noreply.github.com>
2025-04-05 20:51:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const num1 = parseFloat(prompt("Digite o primeiro número:"))
+const num2 = parseFloat(prompt("Digite o segundo número:"))
+
+let resultado = num1 + num2
+alert("Soma: " + resultado)
+
+resultado -= num2
+alert("Subtração: " + resultado)
+
+resultado = num1 * num2
+alert("Multiplicação: " + resultado)
+
+resultado /= num1
+alert("Divisão: " + resultado)
+
+resultado = num1 % num2
+alert("Resto da divisão: " + resultado)
